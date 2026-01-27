c9134e729fdb603f1843b55d689f1d15c7e972e8
Gareth Fong <garethfong@icloud.com>
2026-01-27 16:41:58 +0800

    Update shell configuration

--- modules/shell.nix
+++ modules/shell.nix
@@ -19,6 +19,18 @@
       export PATH="$HOME/bin:/opt/homebrew/bin:/usr/local/bin:$PATH"
       source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
       source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+      
+      # CASE-INSENSITIVE AUTOCOMPLETE
+      autoload -Uz compinit && compinit
+      zstyle ":completion:*" matcher-list "" "m:{a-zA-Z}={A-Za-z}" "r:|=*" "l:|=* r:|=*"
+      
+      # Eclipse-style directory navigation (move up directories)
+      # Usage: type ".." to go up one directory, "..." to go up two, etc.
+      ..() { builtin cd ..; }
+      ...() { builtin cd ../..; }
+      ....() { builtin cd ../../..; }
+      .....() { builtin cd ../../../..; }
+      
      # echo "[ZSH INIT] Loaded by Home Manager"
     '';
   };
